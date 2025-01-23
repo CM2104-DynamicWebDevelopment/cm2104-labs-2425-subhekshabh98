430543f8a90e62f034c5454cc2c1a45106c349a2
subhekshabh98 <115726215+subhekshabh98@users.noreply.github.com>
2025-01-23 11:19:48 +0000

    SEM2 WEE1

--- lab_10/myfirstserver.js
+++ lab_10/myfirstserver.js
@@ -1,9 +1,5 @@
-
 var http = require('http');
-
 http.createServer(function (req, res) {
-
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.end('Hello World!');
- 
-}).listen(8080);
+}).listen(8080); 
