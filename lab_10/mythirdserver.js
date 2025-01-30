686597167a186d9131ebb55ba5c9cb49d8f899d6
Subheksha Bhetuwal <s.bhetuwal@rgu.ac.uk>
2025-01-30 11:00:46 +0000

    ..

--- lab_10/mythirdserver.js
+++ lab_10/mythirdserver.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var knockknock = require('knock-knock-jokes');
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': ‘text/html'});
     var randomJoke = knockknock()
