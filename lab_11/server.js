a12581e5d365837af27c5cdeebb201fd80d02829
Subheksha Bhetuwal <s.bhetuwal@rgu.ac.uk>
2025-01-30 12:17:27 +0000

    ..

--- lab_11/server.js
+++ lab_11/server.js
@@ -47,3 +47,9 @@ app.get('/getform', function(req, res){
      res.send("Hi "+name+" I am sure you will "+quest) ;
     });
     
+    app.post('/postform', function(req, res){
+        var name = req.body.name;
+        var quest = req.body.quest;
+         res.send("Hi "+name+" I am sure you will "+quest) ;
+    });
+    app.use(express.urlencoded({extended:true}))
