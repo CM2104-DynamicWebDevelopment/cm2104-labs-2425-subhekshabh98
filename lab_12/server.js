50d49dbe8a8b155bf4a91976985fb15ba6bad27d
Subheksha Bhetuwal <s.bhetuwal@rgu.ac.uk>
2025-02-06 11:15:07 +0000

    ..

--- lab_12/server.js
+++ lab_12/server.js
@@ -50,4 +50,29 @@ spotifyApi.clientCredentialsGrant().then(
         getTracks('love', res);
     });
    
+    spotifyApi.searchTracks(searchterm)
+        .then(function (data) {
+        var tracks = data.body.tracks.items 
+        //lets set up a empty string to act as the response
+        var HTMLResponse = "";
+        //now lets run through all the items
+        //this is a for loop 
+        for(var i=0; i<tracks.length;i++){
+        
+        var track = tracks[i];
+        console.log(track.name);
+        HTMLResponse = HTMLResponse +
+        "<div>" +
+            "<h2>"+track.name+"</h2>"+
+            "<h4>"+track.artists[0].name+"</h4>"+
+            "<img src='"+track.album.images[0].url +"'>"+
+            "<a href='"+track.external_urls.spotify+"'> Track Details </a>"+
+        "</div>";
+            console.log(HTMLResponse);
+            }
+            res.send(HTMLResponse)
+        }, function (err) {
+            console.error(err);
+        });
+
     app.listen(8080);
